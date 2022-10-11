7c624eb4a8f3cc547e501f77e9a739ae15d58d10
Inieke Sendy <ini.e.sendy@gmail.com>
2022-10-11 12:28:58 +0100

    [feature-nav]: add NavOpen State, setup data-scroll-container and add navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
-import './App.css';
-
+import { useState } from 'react';
+import Navbar from './components/Navbar/Navbar';
+import { Outlet } from 'react-router-dom';
 function App() {
-  return <div className="App"></div>;
+  const [isNavOpen, setIsNavOpen] = useState(false);
+  return (
+    <div
+      className={isNavOpen ? 'main__container nav__open' : 'main__container'}
+      id="main-container"
+      style={
+        isNavOpen
+          ? { height: '100vh', overflow: 'hidden', padding: '0' }
+          : { height: 'auto' }
+      }
+      data-scroll-container
+    >
+      <Navbar isNavOpen={isNavOpen} setIsNavOpen={setIsNavOpen} />
+      <Outlet />
+    </div>
+  );
 }
 
 export default App;
