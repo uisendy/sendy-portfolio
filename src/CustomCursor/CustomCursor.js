8a7cd864b9f0431a2fcc4ec393dfebdb2e8bee21
Inieke Sendy <ini.e.sendy@gmail.com>
2022-10-11 14:36:23 +0100

    [feature-nav]: Add: Custom Cursor

---
+++ src/CustomCursor/CustomCursor.js
@@ -0,0 +1,83 @@
+import React, { useEffect, useRef } from 'react';
+import './style.css';
+const CustomCursor = () => {
+  const secondaryCursor = useRef(null);
+  const positionRef = useRef({
+    mouseX: 0,
+    mouseY: 0,
+    destinationX: 0,
+    destinationY: 0,
+    distanceX: 0,
+    distanceY: 0,
+    key: -1,
+  });
+
+  useEffect(() => {
+    let size = 50;
+    document.addEventListener('mousemove', (event) => {
+      let path = event.composedPath();
+      if (path.some((x) => x.tagName === 'A')) {
+        size = 150;
+      } else if (path.some((x) => x.id === 'more__info')) {
+        size = 400;
+      } else size = 50;
+
+      secondaryCursor.current.style.width = `${size}px`;
+      secondaryCursor.current.style.height = `${size}px`;
+
+      const { clientX, clientY } = event;
+
+      const mouseX = clientX;
+      const mouseY = clientY;
+
+      positionRef.current.mouseX =
+        mouseX - secondaryCursor.current.clientWidth / 2;
+      positionRef.current.mouseY =
+        mouseY - secondaryCursor.current.clientHeight / 2;
+    });
+
+    return () => {};
+  }, []);
+
+  useEffect(() => {
+    const followMouse = () => {
+      positionRef.current.key = requestAnimationFrame(followMouse);
+      const {
+        mouseX,
+        mouseY,
+        destinationX,
+        destinationY,
+        distanceX,
+        distanceY,
+      } = positionRef.current;
+      if (!destinationX || !destinationY) {
+        positionRef.current.destinationX = mouseX;
+        positionRef.current.destinationY = mouseY;
+      } else {
+        positionRef.current.distanceX = (mouseX - destinationX) * 0.1;
+        positionRef.current.distanceY = (mouseY - destinationY) * 0.1;
+        if (
+          Math.abs(positionRef.current.distanceX) +
+            Math.abs(positionRef.current.distanceY) <
+          0.1
+        ) {
+          positionRef.current.destinationX = mouseX;
+          positionRef.current.destinationY = mouseY;
+        } else {
+          positionRef.current.destinationX += distanceX;
+          positionRef.current.destinationY += distanceY;
+        }
+      }
+      if (secondaryCursor && secondaryCursor.current)
+        secondaryCursor.current.style.transform = `translate3d(${destinationX}px, ${destinationY}px, 0)`;
+    };
+    followMouse();
+  }, []);
+  return (
+    <div className={`cursor-wrapper default`}>
+      <div className="secondary-cursor" ref={secondaryCursor}></div>
+    </div>
+  );
+};
+
+export default CustomCursor;
