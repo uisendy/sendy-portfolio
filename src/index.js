d194c64447c5394d9d82a15773e9f197d199833c
Inieke Sendy <ini.e.sendy@gmail.com>
2022-10-11 14:13:34 +0100

    [feature-nav]: Add: Route, render routeProvider and Custom Cursor

--- src/index.js
+++ src/index.js
@@ -2,10 +2,34 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import ErrorPage from './components/ErrorPage/ErrorPage';
+import Home from './pages/home/Home';
+import About from './pages/About/About.js';
+import CustomCursor from './CustomCursor/CustomCursor';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+      {
+        path: '/a-sendyinieke',
+        element: <About />,
+      },
+    ],
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <CustomCursor />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
