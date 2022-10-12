e165850bcc9d212323c3726d1ef47a6d665dd22c
Inieke Sendy <ini.e.sendy@gmail.com>
2022-10-12 02:12:28 +0100

    [feature-nav]: add: Footer + Animation added

---
+++ src/components/Footer/FooterBar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const FooterBar = () => {
+  return (
+    <div className="border-t border-white/10 ">
+      <p id="copyright-tag" className="text-xs leading-relaxed text-center text-gray-300">
+        All rights reserved © 2022.
+        <br />
+        Created by Sendy Inieke.
+      </p>
+    </div>
+  );
+};
+
+export default FooterBar;
