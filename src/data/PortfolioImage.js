39b171aca00ccb73be74cc9061fdb4d32c85c2bd
Inieke Sendy <ini.e.sendy@gmail.com>
2022-10-12 08:47:45 +0100

    [feature-nav]: Add: project details added

---
+++ src/data/PortfolioImage.js
@@ -0,0 +1,76 @@
+const projects = [
+  {
+    id: 1,
+    name: 'Happy Travel',
+    technology: {
+      javascript: 'JS',
+      react: 'REACT',
+      typeScript: 'TS',
+    },
+    image:
+      'https://i.pinimg.com/originals/47/36/72/47367215261f240c2d0d8b9d08532ff1.jpg',
+    description:
+      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cumque dignissimos placeat fuga nihil nemo iusto, velit commodi. Ab facilis quidem expedita vitae facere optio perferendis. Molestias laudantium corporis fugit ex!',
+  },
+  {
+    id: 2,
+    name: 'ArchTek',
+    technology: {
+      javascript: 'JS',
+      react: 'REACT',
+      tailwindcss: 'T',
+      rubyOnRails: 'RUBY',
+      typeScript: 'TS',
+    },
+    image:
+      'https://cdn.psdrepo.com/images/2x/freebie-landing-page-furniture-g8.jpg',
+    description:
+      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cumque dignissimos placeat fuga nihil nemo iusto, velit commodi. Ab facilis quidem expedita vitae facere optio perferendis. Molestias laudantium corporis fugit ex!',
+  },
+  {
+    id: 3,
+    name: 'EatnSmile',
+    technology: {
+      javascript: 'JS',
+      react: 'REACT',
+      tailwindcss: 'T',
+      rubyOnRails: 'RUBY',
+      typeScript: 'TS',
+    },
+    image:
+      'https://d85wutc1n854v.cloudfront.net/live/products/600x375/WB04DG52K.png?v=1.0',
+    description:
+      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cumque dignissimos placeat fuga nihil nemo iusto, velit commodi. Ab facilis quidem expedita vitae facere optio perferendis. Molestias laudantium corporis fugit ex!',
+  },
+  {
+    id: 4,
+    name: 'EatnSmile',
+    technology: {
+      javascript: 'JS',
+      react: 'REACT',
+      tailwindcss: 'T',
+      rubyOnRails: 'RUBY',
+      typeScript: 'TS',
+    },
+    image:
+      'https://d85wutc1n854v.cloudfront.net/live/products/600x375/WB04DG52K.png?v=1.0',
+    description:
+      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cumque dignissimos placeat fuga nihil nemo iusto, velit commodi. Ab facilis quidem expedita vitae facere optio perferendis. Molestias laudantium corporis fugit ex!',
+  },
+  {
+    id: 5,
+    name: 'EatnSmile',
+    technology: {
+      javascript: 'JS',
+      react: 'REACT',
+      tailwindcss: 'T',
+      rubyOnRails: 'RUBY',
+      typeScript: 'TS',
+    },
+    image:
+      'https://d85wutc1n854v.cloudfront.net/live/products/600x375/WB04DG52K.png?v=1.0',
+    description:
+      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cumque dignissimos placeat fuga nihil nemo iusto, velit commodi. Ab facilis quidem expedita vitae facere optio perferendis. Molestias laudantium corporis fugit ex!',
+  },
+];
+export default projects;
