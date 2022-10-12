519fc7db5ac7084c745b063a4b5a29b4bdf5ee14
Inieke Sendy <ini.e.sendy@gmail.com>
2022-10-12 09:31:19 +0100

    [feature-nav]: Add: recentProject and Footer Hooks

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Hero from '../../components/Hero/Hero';
+import RecentProjects from '../../components/RecentProjects/RecentProjects';
+import Footer from '../../components/Footer/Footer';
 
 const Home = () => {
   return (
     <div>
       <Hero />
+      <RecentProjects />
+      <Footer />
     </div>
   );
 };
