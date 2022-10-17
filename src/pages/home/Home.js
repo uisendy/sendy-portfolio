c42fabe51807a7998701ed7b37a3ac3458921e19
Inieke Sendy <ini.e.sendy@gmail.com>
2022-10-17 20:21:03 +0100

    [feature-about]: Add GSAP animation - text

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Hero from '../../components/Hero/Hero';
-import RecentProjects from '../../components/RecentProjects/RecentProjects';
 import Footer from '../../components/Footer/Footer';
+import ScrollToTop from '../../components/ScrollDownIcon/ScrollToTop';
+import ScrollTrigger from 'gsap/ScrollTrigger';
+import gsap from 'gsap';
+import RecentProjects from '../../components/RecentProjects/RecentProjects';
 
 const Home = () => {
+  gsap.registerPlugin(ScrollTrigger);
+  useEffect(() => {
+    <ScrollToTop />;
+    ScrollTrigger.refresh();
+  }, []);
   return (
     <div>
       <Hero />
