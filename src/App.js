dfa3a95818e8c1db787035e7e07f04d8606d42f4
Inieke Sendy <ini.e.sendy@gmail.com>
2022-10-17 20:17:38 +0100

    [feature-about]: Add Welcome Loader

--- src/App.js
+++ src/App.js
@@ -1,22 +1,59 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import NavBar from './components/Navbar/NavBar';
 import { Outlet } from 'react-router-dom';
+import useLocoScroll from './hooks/useLocoScroll';
+import ScrollTrigger from 'gsap/ScrollTrigger';
+import gsap from 'gsap';
+import Loader from './components/Loader';
 function App() {
+  gsap.registerPlugin(ScrollTrigger);
+  const [preLoader, setPreLoader] = useState(false);
   const [isNavOpen, setIsNavOpen] = useState(false);
+
+  useLocoScroll(!preLoader);
+  const [timer, setTimer] = useState(3);
+
+  const id = useRef(null);
+
+  const clear = () => {
+    window.clearInterval(id.current);
+    setPreLoader(false);
+  };
+
+  useEffect(() => {
+    ScrollTrigger.refresh();
+    id.current = window.setInterval(() => {
+      setTimer((timer) => timer - 1);
+    }, 1000);
+  }, []);
+
+  useEffect(() => {
+    if (timer < 0) {
+      clear();
+      setTimer(0);
+    }
+  }, [timer]);
+
   return (
-    <div
-      className={isNavOpen ? 'main__container nav__open' : 'main__container'}
-      id="main-container"
-      style={
-        isNavOpen
-          ? { height: '100vh', overflow: 'hidden', padding: '0' }
-          : { height: 'auto' }
-      }
-      data-scroll-container
-    >
-      <NavBar isNavOpen={isNavOpen} setIsNavOpen={setIsNavOpen} />
-      <Outlet />
-    </div>
+    <>
+      {preLoader ? (
+        <Loader />
+      ) : (
+        <div
+          className={isNavOpen ? 'main__container nav__open' : 'main__container'}
+          id="main-container"
+          style={
+            isNavOpen
+              ? { height: '100vh', overflow: 'hidden', padding: '0' }
+              : { height: 'auto' }
+          }
+          data-scroll-container
+        >
+          <NavBar isNavOpen={isNavOpen} setIsNavOpen={setIsNavOpen} />
+          <Outlet />
+        </div>
+      )}
+    </>
   );
 }
 
