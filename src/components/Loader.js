4f3df8a5e50f9cd8a7a60b780f0c03e37a19f628
Inieke Sendy <ini.e.sendy@gmail.com>
2022-10-17 20:18:27 +0100

    [feature-about]: Create Loader Component

---
+++ src/components/Loader.js
@@ -0,0 +1,10 @@
+import React from 'react';
+const Loader = () => {
+  return (
+    <div className="loader-warper fixed flex flex-col justify-center items-center top-0 right-0 bottom-0 left-0 bg-black">
+      <h1 className="text-white font-playFairSc ">Welcome</h1>
+    </div>
+  );
+};
+
+export default Loader;
