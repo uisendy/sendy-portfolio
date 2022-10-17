b7640d8e04dd5d10c91e0d375e5563b7d54b65de
Inieke Sendy <ini.e.sendy@gmail.com>
2022-10-17 20:18:47 +0100

    [feature-about]: Disable Strict

--- src/index.js
+++ src/index.js
@@ -28,8 +28,8 @@ const router = createBrowserRouter([
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <>
     <CustomCursor />
     <RouterProvider router={router} />
-  </React.StrictMode>,
+  </>,
 );
