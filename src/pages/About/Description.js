2be62c3d5062be48691f409ef9764ffb6f22824e
Inieke Sendy <ini.e.sendy@gmail.com>
2022-10-18 13:03:33 +0100

    [feature-GSAP]: Update: styling for coreValues

--- src/pages/About/Description.js
+++ src/pages/About/Description.js
@@ -19,21 +19,19 @@ const Description = () => {
   const coreValues = coreValuesObj.map((coreValue) => (
     <li
       key={coreValue.id}
-      className="py-5 px-6 border-y grid grid-cols-6 md:grid-cols-4 items-center gap-6 font-light text-4xl"
+      className="font-montserrat py-5 px-6 border-y grid grid-cols-6 md:grid-cols-4 items-center gap-6 font-light text-4xl"
     >
-      <p className=" not-italic font-montserrat text-3xl md:text-5xl lg:text-7xl text-left">
-        {coreValue.no}
-      </p>
-      <p className=" text-3xl col-span-5 md:col-span-3 ">{coreValue.value}</p>
+      <p className="text-3xl md:text-5xl lg:text-7xl text-left">{coreValue.no}</p>
+      <p className=" text-lg lg:text-2xl col-span-5 md:col-span-3 ">{coreValue.value}</p>
     </li>
   ));
 
   return (
-    <div data-scroll-section className=" mb-32">
+    <div data-scroll-section className=" mb-32 px-[6.5%]">
       <SectionHeader title={'brief'} subtitle={'Description & Values'} />
       <div className="description__section md:h-[70vh] grid lg:grid-cols-2 items-center ">
         <div className="flex flex-col gap-8 ">
-          <p className="text-2xl pr-0 lg:pr-[10%] leading-relaxed font-montserrat font-light pb-10 lg:pb-0">
+          <p className=" text-lg lg:text-2xl pr-0 lg:pr-[10%] leading-relaxed font-montserrat font-light pb-10 lg:pb-0">
             Software Engineering is an Art. Consequently, it is my mission to use the
             science of analytical thinking in solving industry problems and identify a
             workable solution that adds value over the long term against a short temporary
