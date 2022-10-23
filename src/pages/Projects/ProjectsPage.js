baf65cd888ba34f82dd9175a741b3b4c12176d6f
Inieke Sendy <ini.e.sendy@gmail.com>
2022-10-24 00:40:23 +0100

    [feature-loader]:Update: ScrollToTop Event pattern changes

--- src/pages/Projects/ProjectsPage.js
+++ src/pages/Projects/ProjectsPage.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ProjectsHero from './ProjectsHero';
 import Projects from '../../components/Projects/Projects';
 import Footer from '../../components/Footer/Footer';
 
 const ProjectsPage = () => {
+  useEffect(() => {
+    window.scrollTo({ top: 0, left: 0 });
+  }, []);
   return (
-    <div>
+    <div style={{ overflowY: 'scroll' }}>
       <ProjectsHero />
       <Projects useHeader={false} />
       <Footer />
