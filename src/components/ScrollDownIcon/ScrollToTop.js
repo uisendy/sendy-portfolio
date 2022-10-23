3ce6dd795cd6c03756e37a0b4e8dcb94306783c1
Inieke Sendy <ini.e.sendy@gmail.com>
2022-10-24 00:39:40 +0100

    [feature-loader]:Update: ScrollToTop Event pattern changes

--- src/components/ScrollDownIcon/ScrollToTop.js
+++ src/components/ScrollDownIcon/ScrollToTop.js
@@ -2,11 +2,9 @@ import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 
 export default function ScrollToTop() {
-  const { pathname } = useLocation();
-
   useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [pathname]);
+    window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
+  }, []);
 
   return null;
 }
