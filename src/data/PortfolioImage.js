c8458587182ac4aa325e1152ec7acdb753308eb3
Inieke Sendy <ini.e.sendy@gmail.com>
2022-10-24 15:54:55 +0100

    [feature-loader]: Add Description and technology to data

--- src/data/PortfolioImage.js
+++ src/data/PortfolioImage.js
@@ -1,76 +1,69 @@
 const projects = [
   {
     id: 1,
-    name: 'Happy Travel',
-    technology: {
-      javascript: 'JS',
-      react: 'REACT',
-      typeScript: 'TS',
-    },
+    name: 'Personal Portfolio',
+    technology: ['JSX', 'react', 'GSAP', 'Locomotive Scroll', 'tailwindcss'],
     image:
-      'https://i.pinimg.com/originals/47/36/72/47367215261f240c2d0d8b9d08532ff1.jpg',
+      'https://images.unsplash.com/photo-1510936111840-65e151ad71bb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1190&q=80',
     description:
-      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cumque dignissimos placeat fuga nihil nemo iusto, velit commodi. Ab facilis quidem expedita vitae facere optio perferendis. Molestias laudantium corporis fugit ex!',
+      'The first Version of my portfolio. This app is simply made to showcase my skills and carries my past projects.',
+    source: '',
+    github: 'https://github.com/uisendy/sendy-portfolio',
   },
   {
     id: 2,
-    name: 'ArchTek',
-    technology: {
-      javascript: 'JS',
-      react: 'REACT',
-      tailwindcss: 'T',
-      rubyOnRails: 'RUBY',
-      typeScript: 'TS',
-    },
+    name: 'Air Quality',
+    technology: ['JSX', 'react', 'Chart JS', 'tailwindcss', 'Pair-Programming', 'jest'],
     image:
-      'https://cdn.psdrepo.com/images/2x/freebie-landing-page-furniture-g8.jpg',
+      'https://images.unsplash.com/photo-1582980752625-10783b273e2d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
     description:
-      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cumque dignissimos placeat fuga nihil nemo iusto, velit commodi. Ab facilis quidem expedita vitae facere optio perferendis. Molestias laudantium corporis fugit ex!',
+      '"AQUA" Air Quality App is an app for tracking air quality for and from any location. It connects to realtime data provided by open weather.This app helps you to stay safe from air pollution in any country, city around the world',
+    source: 'https://aqual.netlify.app',
+    github: 'https://github.com/uisendy/air-quality-app-aqual',
   },
   {
     id: 3,
-    name: 'EatnSmile',
-    technology: {
-      javascript: 'JS',
-      react: 'REACT',
-      tailwindcss: 'T',
-      rubyOnRails: 'RUBY',
-      typeScript: 'TS',
-    },
+    name: 'Space traveler',
+    technology: ['JSX', 'React-Redux', 'tailwindcss', 'Pair-Programming'],
     image:
-      'https://d85wutc1n854v.cloudfront.net/live/products/600x375/WB04DG52K.png?v=1.0',
+      'https://images.unsplash.com/photo-1612349519624-dba4a7ccf707?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80',
     description:
-      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cumque dignissimos placeat fuga nihil nemo iusto, velit commodi. Ab facilis quidem expedita vitae facere optio perferendis. Molestias laudantium corporis fugit ex!',
+      'This is a Minifiy project which provides commercial and scientific space travel services. The Application allows users to book rockets and join selected space missions. It is connected to real live data from the spaceX API',
+    source: 'https://github.com/PRATAP-KUMAR/react-group-project',
+    github: 'https://github.com/PRATAP-KUMAR/react-group-project',
   },
   {
     id: 4,
-    name: 'EatnSmile',
-    technology: {
-      javascript: 'JS',
-      react: 'REACT',
-      tailwindcss: 'T',
-      rubyOnRails: 'RUBY',
-      typeScript: 'TS',
-    },
+    name: 'Bookstore CMS',
+    technology: ['JSX', 'React-Redux', 'tailwindcss', 'AXIOS', 'Jest'],
     image:
-      'https://d85wutc1n854v.cloudfront.net/live/products/600x375/WB04DG52K.png?v=1.0',
+      'https://images.unsplash.com/photo-1623771702313-39dc4f71d275?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
     description:
-      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cumque dignissimos placeat fuga nihil nemo iusto, velit commodi. Ab facilis quidem expedita vitae facere optio perferendis. Molestias laudantium corporis fugit ex!',
+      'The Bookstore is a single page appstore which contains books listed with author and categories of the books. It displays, add and remove books as fnctionalities',
+    source: 'https://sendy-bookstore.netlify.app',
+    github: 'https://github.com/uisendy/react-bookstore',
   },
   {
     id: 5,
-    name: 'EatnSmile',
-    technology: {
-      javascript: 'JS',
-      react: 'REACT',
-      tailwindcss: 'T',
-      rubyOnRails: 'RUBY',
-      typeScript: 'TS',
-    },
+    name: 'Meal Recipe',
+    technology: ['javaScript', 'webpack', 'tailwindcss', 'pair-programming', 'jest'],
     image:
-      'https://d85wutc1n854v.cloudfront.net/live/products/600x375/WB04DG52K.png?v=1.0',
+      'https://images.unsplash.com/photo-1519708227418-c8fd9a32b7a2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
     description:
-      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cumque dignissimos placeat fuga nihil nemo iusto, velit commodi. Ab facilis quidem expedita vitae facere optio perferendis. Molestias laudantium corporis fugit ex!',
+      'GetSen Meal WebApp is a javascript project that has many meal categories and lots of options inside each catagories. We have Imported the data from a MealDB API and implementing an involvement API that allows the user to like each recipe and to add comments to review each of them.',
+    source: 'https://gensen-meal.netlify.app',
+    github: 'https://github.com/uisendy/Meal-webApp',
+  },
+  {
+    id: 6,
+    name: 'Todo List',
+    technology: ['JSX', 'React-Redux', 'tailwindcss', 'jest'],
+    image:
+      'https://images.unsplash.com/photo-1641261689141-ee46b8a0470c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+    description:
+      'In this app, you can add, delete, submit and edit items. This app will persist your data in the browser local storage',
+    source: 'https://sendy-todos.netlify.app/',
+    github: 'https://github.com/uisendy/react-todo-app',
   },
 ];
 export default projects;
